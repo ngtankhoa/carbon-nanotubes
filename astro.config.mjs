4587f6616dad7e0d2855378ae669f678cf648ef6
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2025-02-06 01:21:28 +0700

    dump bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ngtankhoa.github.io',
-  base:'/carbon-nanotubes'
+  base:'/carbon-nanotubes',
   integrations: [mdx(), sitemap()],
 })
